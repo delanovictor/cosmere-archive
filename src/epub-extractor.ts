fc7d6a2dda2c45a09e6d38dc5153cd5e19495ed3
delanovictor <delanovictor3@gmail.com>
2025-03-10 17:31:08 -0300

    split the epub and db logic into two files

---
+++ src/epub-extractor.ts
@@ -0,0 +1,178 @@
+import {readFile, writeFile} from "node:fs/promises"
+import x2j from "simple-xml-to-json"
+import he from "he"
+
+type EpubChapter = {
+    chapterTitle: string,
+    src: string,
+}
+
+type NewEpubExtractor = {
+    folder : string
+}
+
+export class EpubExtractor {
+
+    folder : string
+
+    ITALIC_CLASSES = [
+        `italic`,
+        `ePub-I`,
+        `ePub-SCI`,
+        `cep-I`,
+        `CEP`,
+        `CEP-NOBOT`,
+        `CEPV-NOBOT`,
+        `CEPV`,
+        `ACL`,
+        `ACL1`,
+        `blur`
+    ]
+
+    BOLD_CLASSES = [
+        `bold`,
+        `ePub-B`
+    ]
+
+    constructor(config : NewEpubExtractor){
+
+        if(config.folder.endsWith(`/`)){
+            this.folder = config.folder
+        }else{
+            throw new Error(`folder must end with / (${config.folder})`)
+        }
+    }
+
+
+    public async getChapterListFromTOC(){
+
+        const toc = await this.getTOC()
+        
+        const navMap = this.getChild(toc.ncx.children, `navMap`)
+
+        const chapters : EpubChapter[] = []
+    
+        for(const c of navMap.children){
+            const navPoint = c.navPoint
+            
+            chapters.push(...this.recursiveExtractChapters(navPoint))
+        }
+    
+        return chapters
+    }
+
+    public async getChapterParagraphs(chapter : EpubChapter) : Promise<string[]> {
+             
+        const chapterBuffer = await readFile(`${this.folder}${chapter.src}`)
+
+        const chapterJSON = x2j.convertXML(chapterBuffer.toString())
+    
+        const body = this.getChild(chapterJSON.html.children, `body`)
+    
+        const paragraphs = body.children.map(
+            (el) => this.extractText(el, 0)
+        ) as string[]
+        
+        return paragraphs.filter(p => p.length > 0)
+    }
+
+
+    private async getTOC(){
+        const fileBuffer = await readFile(`${this.folder}toc.ncx`)
+    
+        const docTypeRegex = /<!DOCTYPE .*>/gm;
+
+        const fileText = fileBuffer.toString().replace(docTypeRegex, ``);
+
+        const toc = x2j.convertXML(fileText)
+    
+        await writeFile(`./toc-${Date.now().toString()}.json`, JSON.stringify(toc))
+
+        return toc
+    }
+
+
+    private recursiveExtractChapters(navPoint) : EpubChapter[]{
+        const chapters : EpubChapter[] = []
+
+        const navLabel = this.getChild(navPoint.children, `navLabel`)
+
+        const text =  this.getChild(navLabel.children, `text`)
+
+        const content = this.getChild(navPoint.children, `content`)
+
+        const rootChapter : EpubChapter = {
+            chapterTitle: this.formatString(text.content),
+            src: content.src
+        }
+
+        chapters.push(rootChapter)
+
+        for(const child of navPoint.children){
+        
+            if(child.navPoint){
+
+                const childChapters = this.recursiveExtractChapters(child.navPoint)
+
+                chapters.push(...childChapters)
+            }
+        }
+
+
+        return chapters
+    }
+
+    private extractText(parentElement, depth){
+        let text = ""
+        // console.log()
+        // console.log()
+        // console.log(`[${depth}] Sou o elemento:`, parentElement)
+        
+        for(const key in parentElement){
+
+            const element = parentElement[key]
+            // console.log(`Esse eh um dos meus filhos:`, element)
+
+            if(key == `content`){
+                text += element
+            }
+
+            if(element.children){
+                for(const childElement of element.children){
+                    text +=  this.extractText(childElement, depth++)
+                }
+            }
+
+            if(element.content){
+                if(this.ITALIC_CLASSES.indexOf(element.class) > -1){
+                    text += `<i>${element.content}</i>`    
+                }else if(this.BOLD_CLASSES.indexOf(element.class) > -1){
+                    text += `<b>${element.content}</b>`    
+                }else {
+                    text += element.content 
+                }
+            }
+        }
+
+        return text
+    }
+
+    private formatString(string){
+
+        let formattedString = he.decode(string)
+    
+        formattedString = formattedString.replace(`’`, `'`)
+        
+        return formattedString
+    }
+
+    private getChild(children, childKey){
+        return children.find(c => Object.keys(c).indexOf(childKey) > -1)[childKey]
+    }
+    
+}
+
+
+
+
+
---
+++ src/importer.ts
@@ -0,0 +1,176 @@
+import { EpubExtractor } from "./epub-extractor.ts"
+import {randomUUID} from 'node:crypto'
+import sqlite3 from 'sqlite3'
+
+const db = new sqlite3.Database('./cosmere_archive.db');
+
+type Chapter = {
+    chapter_uuid: string,
+    book_id: string,
+    chapter_index: number,
+    chapter_title: string,
+    src: string,
+}
+
+type Paragraph = {
+    book_id: string,
+    chapter_uuid: string,
+    paragraph_uuid: string,
+    content: string,
+}
+
+type Book = {
+    id: string,
+    folder: string
+}
+
+async function main (){
+    const books : Book[] = [
+        {
+            id: `twok`,
+            folder: `./html/twok/`,
+        },
+        {
+            id: `wor`,
+            folder: `./html/wor/OEBPS/`,
+        },
+        {
+            id: `edge`,
+            folder: `./html/edge/`,
+        },
+        {
+            id: `ob`,
+            folder: `./html/ob/OEBPS`,
+        },
+        {
+            id: `dawn`,
+            folder: `./html/dawn/OEBPS/`,
+        },
+        {
+            id: `row`,
+            folder: `./html/row/`,
+        },
+        {
+            id: `wat`,
+            folder: `./html/wat/`,
+        }
+    ]
+
+    for(const book of books){
+        const BOOK_ID = book.id
+
+        const epubExtractor = new EpubExtractor({folder: book.folder})
+
+        const tocChapters = await epubExtractor.getChapterListFromTOC()
+    
+        const chapterList = tocChapters.filter(c => isValidChapter(c.chapterTitle))
+        
+        for(let i = 0; i < chapterList.length; i++){
+    
+            const chapter = chapterList[i]
+            
+            console.log(`Chapter: ${chapter.chapterTitle}`)
+    
+            const dbChapter = await insertChapter({
+                book_id: BOOK_ID,
+                chapter_index: i,
+                chapter_title: chapter.chapterTitle.toUpperCase(),
+                chapter_uuid: randomUUID(),
+                src: chapter.src
+            })
+    
+            const paragraphs = await epubExtractor.getChapterParagraphs(chapter)
+    
+            const dbParagraphs : Paragraph[] = paragraphs.map((p) => {
+                return {
+                    chapter_uuid: dbChapter.chapter_uuid,
+                    paragraph_uuid: randomUUID(),
+                    book_id: dbChapter.book_id,
+                    content: p
+                }
+            })
+    
+            console.log(`Paragraphs: ${dbParagraphs.length}`)
+    
+            await insertParagraphs(dbParagraphs)
+        }
+    }
+}
+
+
+async function insertParagraphs(paragraphs : Paragraph[]) : Promise<void>{
+    return new Promise((resolve, reject) => {
+        console.log(`Inserindo ${paragraphs.length} paragrafos...`)
+    
+        const stmt = db.prepare("INSERT INTO paragraphs VALUES (?, ?, ?, ?)");
+    
+        for(const paragraph of paragraphs){
+            stmt.run(
+                paragraph.book_id, 
+                paragraph.chapter_uuid, 
+                paragraph.paragraph_uuid, 
+                paragraph.content, 
+            );
+        }
+    
+        stmt.finalize((error)=> {
+            if(error){
+                reject(error)
+            }else{
+                resolve()
+            }
+        });
+    })
+  
+}
+
+async function insertChapter(chapter : Chapter) : Promise<Chapter>{
+    return new Promise((resolve, reject) => {
+
+        const stmt = db.prepare("INSERT INTO chapters VALUES (?, ?, ?, ?)");
+
+        stmt.run(
+            chapter.chapter_uuid, 
+            chapter.book_id, 
+            chapter.chapter_index, 
+            chapter.chapter_title, 
+        );
+
+        stmt.finalize((error)=> {
+            if(error){
+                reject(error)
+            }else{
+                resolve(chapter)
+            }
+        });
+    })
+}
+
+function isValidChapter(chapterTitle){
+
+    if(chapterTitle.indexOf(`Prelude`) > -1){
+        return true
+    }
+
+    if(chapterTitle.indexOf(`Prologue`) > -1){
+        return true
+    }
+
+    if(chapterTitle.indexOf(`Postlude`) > -1){
+        return true
+    }
+
+    if(chapterTitle.indexOf(`Epilogue`) > -1){
+        return true
+    }
+
+    if(/\d/.test(chapterTitle)){
+        return true
+    }
+   
+    return false
+}
+
+export {}
+
+await main()
